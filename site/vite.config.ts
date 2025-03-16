2ad7ad8b9b77f67a956ef4ec71d5811352170660
parkie <69488850+mlodyskiny@users.noreply.github.com>
2025-03-17 00:25:01 +0100

    Update vite.config.ts

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -3,8 +3,12 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './', // Ensures correct relative paths
 	plugins: [react()],
 	server: {
 		allowedHosts: true,
+	},
+	build: {
+		outDir: 'dist', // Make sure the output directory is correct
 	},
 });
